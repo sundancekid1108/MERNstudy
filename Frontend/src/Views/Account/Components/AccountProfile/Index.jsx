ac96d91f4b8cc098a68f82cbf5aeef7b57582d74
sundancekid1108 <sundanekid1108@gmail.com>
2021-06-01 13:59:57 +0900

    Profile작업

--- Frontend/src/Views/Account/Components/AccountProfile/Index.jsx
+++ Frontend/src/Views/Account/Components/AccountProfile/Index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames";
 import moment from "moment";
 import { withStyles } from "@material-ui/core";
@@ -12,8 +13,50 @@ import {
 // Component styles
 import styles from "./Styles";
 
-const AccountProfile = () => {
-  return <></>;
+const AccountProfile = (props) => {
+  const { user, classes, className, ...rest } = props;
+  const rootClassName = classNames(classes.root, className);
+  return (
+    <>
+      <Portlet {...rest} className={rootClassName}>
+        <PortletContent>
+          <div className={classes.details}>
+            <div className={classes.info}>
+              <Typography variant="h2">
+                {user.firstname + " " + user.lastname}
+              </Typography>
+              <Typography className={classes.emailText} variant="body1">
+                {user.email}
+              </Typography>
+              <Typography className={classes.dateText} variant="body1">
+                Join at: {moment(user.createdAt).format("MM/DD/YYYY")}
+              </Typography>
+            </div>
+            <Avatar
+              className={classes.avatar}
+              src="/images/avatars/avatar.png"
+            />
+          </div>
+        </PortletContent>
+        <PortletFooter>
+          <Button
+            className={classes.uploadButton}
+            color="primary"
+            variant="text"
+          >
+            Upload picture
+          </Button>
+          <Button variant="text">Remove picture</Button>
+        </PortletFooter>
+      </Portlet>
+    </>
+  );
+};
+
+AccountProfile.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(AccountProfile);
