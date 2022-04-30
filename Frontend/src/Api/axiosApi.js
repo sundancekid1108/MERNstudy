907483db524955ff37dd8772e2034041ff13296f
sundancekid1108 <sundancekid1108@gmail.com>
2022-04-30 17:13:09 +0900

    포트 충돌 에러 해결
    5000 =>3001로 포트 변경

--- Frontend/src/Api/axiosApi.js
+++ Frontend/src/Api/axiosApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://localhost:3001/',
     headers: {
         'Content-type': 'application/json'
     }
--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan('combined'));
 app.disable('x-powered-by');
@@ -24,7 +24,7 @@ app.use(
     cors({
         origin: true,
         credentials: true,
-    }),
+    })
 );
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
@@ -36,20 +36,17 @@ app.use(
             httpOnly: true,
             secure: false,
         },
-    }),
+    })
 );
 
 app.use((req, res, next) => {
     // CORS에 x-access-token이 추가되었습니다. jwt로 생성된 토큰은 header의 x-access-token 항목을 통해 전달
     // CORS(Cross-Origin Resource Sharing): 한 도메인에서 로드되어 다른 도메인에 있는 리소스와 상호 작용하는 클라이언트 웹 애플리케이션에 대한 방법을 정의
     // res.header('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization',
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization'
     );
     next();
 });
